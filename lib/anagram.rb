6e7f5cc69897edf65c39bfdb9c6655cf761e06c8
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-05-18 18:44:06 +0300

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+
+class Anagram
+    def initialize(word)
+        @to_check = word.downcase
+        @result =[]
+    end
+
+    def match(arr)
+        arr.each do |word|
+            sorted_word_array = word.downcase.chars.sort
+            # puts sorted_word_array
+            if @to_check.chars.sort == sorted_word_array
+                @result << word
+            end
+        end
+        @result
+    end 
+end
+
+
+listen = Anagram.new('listen')
+matches = listen.match(%w[enlists google inlets banana])
+puts matches
+pp (["dog", "cat"])
